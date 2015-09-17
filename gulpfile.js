fbabc37a99f3d076c9080adec278e4df402d5c30
Lawrence Luo <lawrenceluo198812@gmail.com>
2015-09-17 15:46:28 -0500

    remove library files
    remove files in library to prevent plato to analyze these files

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,19 @@ function callback(report){
   console.log('Report has been generated.');
 }
 
-var
-  gulp = require('gulp');
+var gulp = require('gulp');
 
-// Create the gulp task
 gulp.task('pondering-muse', function() {
   var
     files,
     outputDir,
     plato = require('plato'),
     few = require('front-end-work-flow/gulp-tasks/setup.js');
-    //files = process.cwd() + '/app/**/*.js',
-    //outputDir = process.cwd() + '/output/plato';
   files = few.files( 'browser' )
     .concat( few.exclude( 'unit' ) )
     .concat( few.exclude( 'node' ) )
-    .concat( few.exclude( 'libraries' ) )
     .concat( few.exclude( 'devLibraries' ) );
-  console.log(files);
   few.defaults.platoPath = 'output/plato';
-  //few.config('platoPath');
   outputDir = process.cwd() + '/' + few.config('platoPath');
-  console.log(outputDir);
-  return plato.inspect(files, process.cwd() + '/output/dirt', {}, callback);
+  return plato.inspect(files, outputDir, {}, callback);
 });
